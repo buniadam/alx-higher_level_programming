407f3a332f1c2c3590077f98a2ed9f6f519b6354
buniadam <buniadam24@gmail.com>
2023-02-14 07:16:45 -0800

    task

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `second_table` (`id` INT,
 `score` INT);
 INSERT INTO `second_table` (`id`,`name`, `score`)
 VALUES
-(1,"Peter", 10),
-(2,"Ayo", 3),
-(3,"ojay", 14),
-(4,"Buni", 8);
+(1,"Buni", 10),
+(2,"Ojay", 3),
+(3,"Joy", 14),
+(4,"Peter", 8);
