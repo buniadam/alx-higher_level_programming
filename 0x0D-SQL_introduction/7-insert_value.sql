8374c01d99f5fe1b541745327e0149dc9c8302da
buniadam <buniadam24@gmail.com>
2023-02-14 07:06:08 -0800

    task

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- inserts
-INSERT INTO first_table(id, name) VALUES(89,'Holberton School');
+-- inserts a new row in table first_table
+-- in database hbtn_0c_0
+INSERT INTO first_table(id, name) VALUES (89, 'Best School');
