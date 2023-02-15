464af7ce818a5ee412d2382de75ef19a26bbc158
buniadam <buniadam24@gmail.com>
2023-02-15 13:36:06 -0800

    task

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -2,7 +2,7 @@
 -- id INT default 1 unique, name VARCHAR(256)
 -- If table already exists, script should not fail
 CREATE TABLE IF NOT EXISTS unique_id
-{
-        id INT DEFAULT 1 UNIQUE,
+(
+	id INT DEFAULT 1 UNIQUE,
 	name VARCHAR(256)
 );
