2baf9bb66019d21680cd017630e881ba1e1977e2
buniadam <buniadam24@gmail.com>
2023-02-15 13:54:08 -0800

    task

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -7,8 +7,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- name VARCHAR(256) not null
 -- If table already exists, script should not fail
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
-{
-        id INT UNIQUE AUTO_INCREMENT NOT NULL,
+(
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id)
 );
